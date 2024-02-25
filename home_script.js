307f5a9abe8df3aba0e60a3a44e3ed10ba3ae89f
EmmaArthur <e.arthur@amalitech.org>
2024-02-25 22:30:58 +0000

    fix mobile view

--- home_script.js
+++ home_script.js
@@ -2,9 +2,12 @@ $(function() {
     $("input[type=text]").click(function() {
         $(".placeholder").css("display", "none");
     })
-    $("input[type=text]").blur(function() {
-        $(".placeholder").css("display", "block");
-    })
+    if ($("input[type=text]").blur == true && $input.value == false){
+        $("input[type=text]").blur(function() {
+            $(".placeholder").css("display", "block");
+        })
+    }
+
     $("input[type=email]").click(function() {
         $(".placeholder-email").css("display", "none");
     })
